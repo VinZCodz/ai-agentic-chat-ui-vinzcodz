82aee0d71a0f597423809e3328513dbd468785bc
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2026-01-03 08:17:20 +0000

    Vercel Deploy: CI/CD to Vercel for the UI

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
     name: "VinZ-ChatZ",
     description: "Drive analytics with AI Agents!",
-    url: "https://vinz-chatz.vercel.app",
+    url: "https://vinzcodz-chat-ui.vercel.app",
     ogImage: "/icon.png",
     links: {
         github: "https://github.com/VinZCodz/ai-agentic-chat-ui-vinzcodz",
